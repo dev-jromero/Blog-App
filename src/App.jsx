803ac716f608c0a1750708bfbb6f8bc0da3ae807
Jhonatan Romero <n.bjhon@gmail.com>
2025-09-03 18:59:19 -0500

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <div className='px-4 md: px-8 lg:px-16 lx:px-32 2xl:px-64'>
+        {/* NAVBAR */}
+        <Navbar/>
+        {/* BREADCRUMB */}
+        {/* INTRODUCTION */}
+        {/* FEATURED POST */}
+        {/* POST LIST */}
+      </div>
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+function Navbar() {
+    const [open, setOpen]= useState(false)
+    return (
+        <div className="w-full h-16 md:h-20 flex items-center justify-between">
+            {/* LOGO */}
+            <div className="flex items-center gap-4 text-2xl font-bold">
+                <img src="/logo.png" className="w-8 h-8" alt="" />
+                <span>lamablog</span>
+            </div>
+            {/* MOBILE MENU */}
+            <div className="md:hidden">
+                {/* MOBILE MENU */}
+                <div className="cursor-pointer text-4xl" onClick={()=> setOpen(prev=>!prev)}>
+                    {open ? "X": "="}
+                </div>
+                {/* MOBLIE LINK LIST */}
+                <div className={`w-full h-screen flex flex-col items-center justify-center absolute top-16 transition-all ease-in-out ${open ? "-right-0":"-right-[100%]"}`}>
+                    menu
+                </div>
+            </div>
+            {/* DESKTOP MENU */}
+            <div className="hidden md:flex">D</div>
+        </div>
+    )
+}
+
+export default Navbar
