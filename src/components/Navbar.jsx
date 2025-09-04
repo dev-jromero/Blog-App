a013a0f50258df3b74f0f4fe00da610ac14051ce
Jhonatan Romero <n.bjhon@gmail.com>
2025-09-03 19:45:16 -0500

    add menus

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,12 +16,27 @@ function Navbar() {
                     {open ? "X": "="}
                 </div>
                 {/* MOBLIE LINK LIST */}
-                <div className={`w-full h-screen flex flex-col items-center justify-center absolute top-16 transition-all ease-in-out ${open ? "-right-0":"-right-[100%]"}`}>
-                    menu
+                <div className={`w-full h-screen flex flex-col items-center gap-8 font-medium justify-center absolute top-16 transition-all ease-in-out ${open ? "-right-0":"-right-[100%]"}`}>
+                    <a href="/">Home</a>
+                    <a href="/">Trending</a>
+                    <a href="/">Most Popular</a>
+                    <a href="/">About</a>
+                    <a href="">
+                        <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">Login</button>
+                    </a>
                 </div>
             </div>
             {/* DESKTOP MENU */}
-            <div className="hidden md:flex">D</div>
+            <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+                <a href="/">Home</a>
+                <a href="/">Trending</a>
+                <a href="/">Most Popular</a>
+                <a href="/">About</a>
+                <a href="">
+                    <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">Login</button>
+                </a>
+
+            </div>
         </div>
     )
 }
