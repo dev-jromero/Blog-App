813419ea55745e96077c081b3f3aae8cfa1f2cb7
Jhonatan Romero <n.bjhon@gmail.com>
2025-09-22 18:32:11 -0500

    add comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,9 +1,21 @@
-
-
+import Image from "./Images"
 function Comment() {
     return (
-        <div>
-        
+        <div className="p-4 bg-slate-50 rounded-xl mb-8">
+            <div className="flex items-center gap-4">
+                <Image src="userImg.jpeg" className="w-10 h-10 rounded-full object-cover" w="40" />
+                <span className="font-medium">John Doe</span>
+                <span className="text-sm text-gray-500 ">2 days ago</span>
+            </div>
+            <div className="mt-4 ">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Magnam aliquam doloremque excepturi possimus nemo sed 
+                    commodi adipisci reiciendis. Magnam aliquam doloremque 
+                    excepturi possimus nemo sed commodi adipisci reiciendis.
+                    Magnam aliquam doloremque excepturi possimus nemo sed
+                </p>
+            </div>
         </div>
     )
 }
