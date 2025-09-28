f0754b603c855463349783865ec692bafe2f72c9
Jhonatan Romero <n.bjhon@gmail.com>
2025-09-27 19:07:36 -0500

    Crud blogApp

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,10 @@ import connectDB from "./lib/connectDB.js"
 import userRouter from "./routes/user.route.js"
 import postRouter from "./routes/post.route.js"
 import commentRouter from "./routes/comment.route.js"
+
+
 const app=express()
+app.use(express.json())
 
 // app.get("/test", (req, res)=>{
 //     res.status(200).send("it works!")
